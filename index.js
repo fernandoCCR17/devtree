b38f292a9ca624de54fd75d41bbdc686c79848ae
Fernando de la Cruz Ramos <fernan654happy@gmail.com>
2025-05-14 22:06:53 -0600

    Levantando el servidor y primeros pasos con el router

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo desde node")
+import express from "express";
+
+const app = express();
+
+//Routing
+app.get("/", (req, res) => {
+    res.send("Hola mundo en Express")
+})
+
+const puerto = process.env.PORT ?? 4000;
+
+app.listen(puerto, () => {
+    console.log("Servidor funcionando");
+});
